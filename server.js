25fac267b6399025eabd171e3d570009f9970d66
Kelvin Lu <kelvin.lu.sj@gmail.com>
2016-02-23 16:00:16 -0800

    fleshed out and debugged Angular with update to beta.7

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require('express');
-var path = require('path');
-// var bodyParser = require('body-parser');
+// var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
-app.use(express.static(path.join(__dirname, './client')));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
+// app.use(express.static(path.join(__dirname, './client')));
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-// require('./config/mongoose');
-// require('./config/routes')(app);
+require('./config/mongoose');
+require('./config/routes')(app);
 
 app.listen(7777, function () {
   console.log('ng2-notes running on port 7777');
